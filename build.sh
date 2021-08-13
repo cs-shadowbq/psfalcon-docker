3684cb6d023a0458aa29c7de45afdc4f8c38efad
cs-shadowbq <scott.macgregor@crowdstrike.com>
2021-08-13 12:35:05 -0400

    fix gnu-sed issue

--- build.sh
+++ build.sh
@@ -4,9 +4,25 @@ set -ex
 source image.config
 VERSION=$(<./VERSION)
 
-sed -i '' "/LABEL version=/s/.*/LABEL version=\"${VERSION}\"/" ./Dockerfile
-sed -i '' "/LABEL psfalcon=/s/.*/LABEL psfalcon=\"${PSFALCON}\"/" ./Dockerfile
-sed -i '' "s/RequiredVersion [0-9\.]*/RequiredVersion ${PSFALCON}/" ./Dockerfile
+function is_gnu_sed(){
+  sed --version >/dev/null 2>&1
+}
+
+function sed_i_wrapper(){
+    if is_gnu_sed; then
+        $(which sed) "$@"
+    else
+        a=()
+        for b in "$@"; do
+            [[ $b == '-i' ]] && a=("${a[@]}" "$b" "") || a=("${a[@]}" "$b")
+        done
+        $(which sed) "${a[@]}"
+    fi
+}
+
+sed_i_wrapper -i "/LABEL version=/s/.*/LABEL version=\"${VERSION}\"/" ./Dockerfile
+sed_i_wrapper -i "/LABEL psfalcon=/s/.*/LABEL psfalcon=\"${PSFALCON}\"/" ./Dockerfile
+sed_i_wrapper -i "s/RequiredVersion [0-9\.]*/RequiredVersion ${PSFALCON}/" ./Dockerfile
 
 echo "building version: latest"
 docker build . --file ./Dockerfile -t $USERNAME/$IMAGE:latest
