754e3402bbb43b05c8c62c956ef1ef24f286c848
cs-shadowbq <scott.macgregor@crowdstrike.com>
2021-08-13 12:29:07 -0400

    explicit local paths

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -ex
 source image.config
 VERSION=$(<./VERSION)
 
-sed -i '' "/LABEL version=/s/.*/LABEL version=\"${VERSION}\"/" Dockerfile
-sed -i '' "/LABEL psfalcon=/s/.*/LABEL psfalcon=\"${PSFALCON}\"/" Dockerfile
-sed -i '' "s/RequiredVersion [0-9\.]*/RequiredVersion ${PSFALCON}/" Dockerfile
+sed -i '' "/LABEL version=/s/.*/LABEL version=\"${VERSION}\"/" ./Dockerfile
+sed -i '' "/LABEL psfalcon=/s/.*/LABEL psfalcon=\"${PSFALCON}\"/" ./Dockerfile
+sed -i '' "s/RequiredVersion [0-9\.]*/RequiredVersion ${PSFALCON}/" ./Dockerfile
 
 echo "building version: latest"
-docker build . --file Dockerfile -t $USERNAME/$IMAGE:latest
+docker build . --file ./Dockerfile -t $USERNAME/$IMAGE:latest
