c973d2b193f8a8727780eb0b88f6a59a60168b77
cs-shadowbq <scott.macgregor@crowdstrike.com>
2021-08-18 15:42:05 -0400

    updates oci labels, cross-build ubi8

--- build.sh
+++ build.sh
@@ -20,9 +20,30 @@ function sed_i_wrapper(){
     fi
 }
 
-sed_i_wrapper -i "/LABEL org.opencontainers.image.version=/s/.*/LABEL org.opencontainers.image.version=\"${VERSION}\"/" ./Dockerfile
-sed_i_wrapper -i "/LABEL com.github.cs-shadowbq.psfalcon=/s/.*/LABEL com.github.cs-shadowbq.psfalcon=\"${PSFALCON}\"/" ./Dockerfile
-sed_i_wrapper -i "s/RequiredVersion [0-9\.]*/RequiredVersion ${PSFALCON}/" ./Dockerfile
+docker_kv() {
+    case $1 in
+        'Dockerfile') echo 'ubuntu.20.04';;
+        'Dockerfile.ubuntu.20.04') echo 'ubuntu.20.04';;
+        'Dockerfile.ubi8') echo 'ubi8';;
+        'Dockerfile.alpine') echo 'alpine';;
+        *) echo '';;
+    esac
+}
 
-echo "building version: latest"
-docker build . --file ./Dockerfile -t $USERNAME/$IMAGE:latest
+
+bases=( Dockerfile Dockerfile.ubi8 )
+for base in "${bases[@]}"
+do
+    sed_i_wrapper -i "/LABEL org.opencontainers.image.version=/s/.*/LABEL org.opencontainers.image.version=\"${VERSION}\"/" "./$base"
+    sed_i_wrapper -i "/LABEL com.github.cs-shadowbq.psfalcon=/s/.*/LABEL com.github.cs-shadowbq.psfalcon=\"${PSFALCON}\"/" "./$base"
+    sed_i_wrapper -i "s/RequiredVersion [0-9\.]*/RequiredVersion ${PSFALCON}/" "./$base"
+
+    echo "building version: latest"
+    ostarget=$(docker_kv "$base")
+    docker build . --file "./$base" -t $USERNAME/$IMAGE:latest-"$ostarget" --build-arg IMAGE_CREATE_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+
+done
+
+
+echo "Tagging version (default ubuntu.20.04): latest"
+docker tag $USERNAME/$IMAGE:latest-"ubuntu.20.04" $USERNAME/$IMAGE:latest
